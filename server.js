58dfcef1a687d1cb96a7aa74f7f5afe8854940ef
[Josiah Cook] <[cookj39@matc.edu]>
2025-09-11 19:18:35 -0500

    changes to server.js

--- server.js
+++ server.js
@@ -7,10 +7,28 @@ const app = express();
 // Connect to the database
 connectDatabase();
 
+// Configure Middleware
+app.use(express.json({ extended: false}));
+
 // API endpoints
-app.get('/', (req, res) => {
+/**
+ * @route Get /
+ * @desc Test endpoint
+ */
+app.get('/', (req, res) => 
     res.send('http get request sent to root api endpoint')
+);
+
+/**
+ * @route POST api/users
+ * @desc Register user
+ */
+app.post('/api/users', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
 });
+
+
 
 // Connection listener
 app.listen(3000, () => console.log('Express server running on port 3000'));
