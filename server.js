b56973e5126b950c97d2c533e5a552a96991c271
[Josiah Cook] <[cookj39@matc.edu]>
2025-09-11 20:09:22 -0500

    Test data validation using Postman

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import connectDatabase from './config/db.js';
+import {check, validationResult } from 'express-validator';
 
 // Initialize express application
 const app = express();
@@ -23,9 +24,20 @@ app.get('/', (req, res) =>
  * @route POST api/users
  * @desc Register user
  */
-app.post('/api/users', (req, res) => {
-    console.log(req.body);
-    res.send(req.body);
+app.post(
+    '/api/users', 
+    [
+        check('name', 'Please enter your name').not().isEmpty(),
+        check('email', 'Please enter a valid email').isEmail(),
+        check('password', 'Please enter a password with 6 or more characters').isLength({min: 6})
+    ],  (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        } else {
+            //Later we will save the user to the database here
+            return res.send(req.body);
+        }
 });
 
 
